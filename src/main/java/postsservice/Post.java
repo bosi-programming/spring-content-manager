72f94adf2415e2e322bff2e5e0eb294724da0be6
fbosi <bosisoftwareengineering@protonmail.com>
2020-12-12 15:47:07 -0300

    Add mongo connection

---
+++ src/main/java/postsservice/Post.java
@@ -0,0 +1,47 @@
+package postsservice;
+
+import java.time.LocalDate;
+import org.springframework.data.annotation.Id;
+
+public class Post {
+  @Id
+  private String id;
+
+  private String author;
+  private LocalDate date;
+  private String title;
+  private String content;
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
---
+++ src/main/java/postsservice/PostRepository.java
@@ -0,0 +1,12 @@
+package postsservice;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "posts", path = "posts")
+public interface PostRepository extends MongoRepository<Post, String> {
+  List<Post> findByTitle(@Param("title") String title);
+}
